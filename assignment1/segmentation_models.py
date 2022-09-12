3ff2767038f30d833cddcdea177e22069b7ea392
Daniel <daniel.stangeland@outlook.com>
2022-09-12 14:40:21 +0200

    finished the code for assignment 1. Still need to answer the questions

--- assignment1/segmentation_models.py
+++ assignment1/segmentation_models.py
@@ -2,7 +2,6 @@ import tensorflow as tf
 from tensorflow.keras import layers, models
 
 def simple_model(input_shape):
-
     height, width, channels = input_shape
     image = layers.Input(input_shape)
     x = layers.Conv2D(32, 5, strides=(2, 2), padding='same', activation='relu')(image)
@@ -16,52 +15,53 @@ def simple_model(input_shape):
 
     return model
 
-def conv2d_3x3(filters):
-    conv = layers.Conv2D(
-        filters, kernel_size=(3, 3), activation='relu', padding='same'
-    )
-    return conv
-
-def up_conv(image, filter1, filter2):
-    c = conv2d_3x3(filter1)(image)
-    c = conv2d_3x3(filter2)(c)
-    p = layers.UpSampling2D(
-        filter2, kernel_size=(2,2),padding='same'
-    )
-    return c, p
-
+def conv2d_3x3(filter):
+    return layers.Conv2D(filter, kernel_size=(3, 3), activation='relu', padding='same')
 
 def max_pool():
     return layers.MaxPooling2D((2, 2), strides=2, padding='same')
 
-def down_conv(image, filters):
-    c = conv2d_3x3(filters)(image)
-    c = conv2d_3x3(filters)(c)
+def down_conv(image, filter):
+    c = conv2d_3x3(filter)(image)
+    c = conv2d_3x3(filter)(c)
     p = max_pool()(c)
 
     return c, p
 
+def up_conv(image, filter):
+    c = conv2d_3x3(filter*2)(image)
+    c = conv2d_3x3(filter*2)(c)
+    
+    return layers.Conv2DTranspose(filter, kernel_size=(2,2), strides=2, padding='same')(c)
+
+
+def output_seg(image):
+    c = conv2d_3x3(8)(image)
+    c = conv2d_3x3(8)(c)
+
+    return layers.Conv2D(1, kernel_size=(1, 1), activation='sigmoid')(c)
+
 def unet(input_shape):
-
     image = layers.Input(shape=input_shape)
-
-    #Down
+    
+    #Downwards
     c1, p1 = down_conv(image, 8)
     c2, p2 = down_conv(p1, 16)
     c3, p3 = down_conv(p2, 32)
     c4, p4 = down_conv(p3, 64)
 
+    #Bottom
+    c5 = up_conv(p4, 64)
 
-    #Up
-    c5, p5 = up_conv(c4, p4, 128, 64)
-    
+    #Upwards
+    c6 = up_conv(tf.concat([c4, c5], 3), 32)
+    c7 = up_conv(tf.concat([c3, c6], 3), 16)
+    c8 = up_conv(tf.concat([c2, c7], 3), 8)
 
-
-    # Fill the layers from 2 to 9.
-    # .........................
-
-    probs = layers.Conv2D(1, kernel_size=(1, 1), activation='sigmoid')(c9)
+    #Output segment
+    probs = output_seg(tf.concat((c1,c8),3))
 
     model = models.Model(inputs=image, outputs=probs)
+    model.summary()
 
     return model
